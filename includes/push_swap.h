a5fa516f1b67736ad44b8b3fba9914f361163279
krozis <stelie@student.42.fr>
2022-05-16 23:23:00 +0200

    early push

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 17:31:48 by krozis            #+#    #+#             */
-/*   Updated: 2022/05/16 20:10:36 by krozis           ###   ########.fr       */
+/*   Updated: 2022/05/16 20:44:46 by krozis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ typedef struct s_ps
 {
 	int	*tab_a;
 	int	*tab_b;
-	
-};
+	int len;
+}				t_ps;
 
 
 #endif
