88217c9ad9e9607e325a3032e342e32dea81d91e
krozis <stelie@student.42.fr>
2022-05-25 18:40:47 +0200

    chunk working good

--- src/ps_tools.c
+++ src/ps_tools.c
@@ -6,7 +6,7 @@
 /*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 18:02:10 by krozis            #+#    #+#             */
-/*   Updated: 2022/05/25 13:53:11 by krozis           ###   ########.fr       */
+/*   Updated: 2022/05/25 18:38:47 by krozis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,25 @@
 
 void	ps_chunker(t_ps *ps)
 {
-	int	i;
-	int	max_in_chunk;
-
+	int i;
+	int	add;
+	int div;
+	int mod;
+	
 	i = 0;
-	max_in_chunk = 0;
+	add = 1;
+	div = ps->len / ps->chunk;
+	mod = ps->len % ps->chunk;
+	ft_printf("number of int = %i\n", ps->len);
 	while (i < ps->chunk)
 	{
-		max_in_chunk += ps->len / ps->chunk;
-		ps->lmt[i] = max_in_chunk;
+		ps->lmt[i] = div * (i + 1);
+		if (i >= ps->chunk - mod)
+		{
+			ps->lmt[i] += add;
+			add++;
+		}
+		ft_printf("CHUNK %i (limit = %i)\n", i + 1, ps->lmt[i]);
 		i++;
 	}
 }
